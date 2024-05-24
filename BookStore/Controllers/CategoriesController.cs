6af052fc66bd7a8cf3924f8cfc33ffa3c9940d71
SanaEhab <sana.aljoubeh@gmail.com>
2024-05-24 16:30:54 +0300

    Create Category

---
+++ BookStore/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using BookStore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var category = context.Categories.ToList();
+            return View(category);
+        }
+    }
+}
