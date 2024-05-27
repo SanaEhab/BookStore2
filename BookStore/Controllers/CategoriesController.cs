dd134bdf3980bf2be528e496a3ba26e4f34a1e9b
SanaEhab <sana.aljoubeh@gmail.com>
2024-05-28 01:18:46 +0300

    CRUD

--- BookStore/Controllers/CategoriesController.cs
+++ BookStore/Controllers/CategoriesController.cs
@@ -26,7 +26,7 @@ namespace BookStore.Controllers
         }
         [HttpPost]
         public IActionResult Create(CategoryVM categoryVM) { 
-
+            //model state used for validation(makes validation without request to the data base)
             if(!ModelState.IsValid)
             {
                 return View("Create", categoryVM);
@@ -41,18 +41,73 @@ namespace BookStore.Controllers
         }
 
         [HttpGet]
-        public IActionResult Edit()
+        public IActionResult Edit(int id)
         {
-            return View("Create");
+            //to save the data into the input element when i click on edit
+            var category = context.Categories.Find(id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new CategoryVM
+            {
+                Id = id,
+                Name = category.Name,
+            };
+            return View("Create",viewModel);
         }
 
         [HttpPost]
         public IActionResult Edit(CategoryVM categoryVM)
         {
+            if(!ModelState.IsValid)
+            {
+                return View("Create",categoryVM);
+            }
+            //take the data from the view and update the data base
             var category = context.Categories.Find(categoryVM.Id);
+
+            if(category is null)
+            {
+                //notfound is build in 
+                return NotFound();
+            }
             category.Name = categoryVM.Name;
+            //when i make any updates the updated on will take the update time
+            category.UpdatedOn = DateTime.Now;
             context.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            var categories = context.Categories.Find(id);
+            if (categories is null)
+            {
+                return NotFound();
+            }
+            var categoryVM = new CategoryVM
+            {
+                Id = id,
+                Name = categories.Name,
+                CreatedOn = categories.CreatedOn,
+                UpdatedOn = categories.UpdatedOn,
+            };
+            return View(categoryVM);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var categories = context.Categories.Find(id);
+            if(categories is null)
+            {
+                return NotFound();
+            }
+            context.Categories.Remove(categories);
+            context.SaveChanges();
+            //build in ok return status200 which means that the requset is done
+            return Ok();
         }
     }
 }
--- BookStore/ViewModel/CategoryVM.cs
+++ BookStore/ViewModel/CategoryVM.cs
@@ -8,6 +8,8 @@ namespace BookStore.ViewModel
 
         [Required(ErrorMessage="please enter name")]
         [MaxLength(30, ErrorMessage = "the max char equal 30")]
-        public string Name { get; set; } = null!;
+        public string Name { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public DateTime UpdatedOn { get; set; } = DateTime.Now;
     }
 }
